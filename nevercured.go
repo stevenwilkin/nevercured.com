b866803fb357694e289b0e9016c8b235efed9ea1
Steven Wilkin <stevenwilkin@gmail.com>
2014-08-13 22:53:14 +0100

    Populate template from db using a hardcoded id

--- nevercured.go
+++ nevercured.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 	"html/template"
 	"net/http"
 	"os"
 	"path"
+	"time"
 )
 
+type Entry struct {
+	Month   string
+	Day     int
+	Title   string
+	Excerpt string
+	Source  string
+	Content template.HTML
+	Summary string
+}
+
 var (
+	db   *sql.DB
 	port int
 	tmpl *template.Template
 )
@@ -21,10 +35,38 @@ func init() {
 	pwd, _ := os.Getwd()
 	filename := path.Join(pwd, "templates", "index.tmpl")
 	tmpl, _ = template.ParseFiles(filename)
+
+	var err error
+	db, err = sql.Open("sqlite3", "./db/nevercured.db")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func getEntry() Entry {
+	sql := "SELECT * FROM jft WHERE id = 366"
+
+	var id int
+	var month int
+	var day int
+	var title string
+	var excerpt string
+	var source string
+	var content string
+	var summary string
+
+	err := db.QueryRow(sql).Scan(&id, &month, &day, &title, &excerpt, &source,
+		&content, &summary)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return Entry{time.Month(month).String(), day, title, excerpt, source,
+		template.HTML(content), summary}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	tmpl.Execute(w, struct{}{})
+	tmpl.Execute(w, getEntry())
 }
 
 func main() {
